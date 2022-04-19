41139a817d910a651882506b1210d05648928544
JiYoungPark <aksend82@daum.net>
2022-04-19 11:37:19 +0900

    DsSetFirstCdAction 커밋

---
+++ source/ActionAssetProject/PopupCloseAction.js
@@ -0,0 +1,34 @@
+﻿//==============================================================================
+//	Define the Action.
+//==============================================================================
+//==============================================================================		
+// Object : nexacro.PopupCloseAction		
+// Group : Action		
+//==============================================================================		
+if (!nexacro.PopupCloseAction)		
+{		
+    nexacro.PopupCloseAction = function(id, parent)		
+    {		
+        nexacro.Action.call(this, id, parent);		
+    };		
+        		
+    nexacro.PopupCloseAction.prototype = nexacro._createPrototype(nexacro.Action, nexacro.PopupCloseAction);		
+    nexacro.PopupCloseAction.prototype._type_name = "PopupCloseAction";		
+	
+	//===============================================================		
+    // nexacro.PopupCloseAction : Create & Destroy		
+    //===============================================================		
+    nexacro.PopupCloseAction.prototype.destroy = function()		
+	{	
+		nexacro.Action.prototype.destroy.call(this);
+	};	
+		
+    //===============================================================		
+    // nexacro.PopupCloseAction : Method		
+    //===============================================================		
+    nexacro.PopupCloseAction.prototype.run = function()		
+	{	
+        //TODO		
+	};	
+	
+}
