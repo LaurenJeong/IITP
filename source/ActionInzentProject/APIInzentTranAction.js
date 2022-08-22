1c5e33b2b31d650259a75e0e6149d6ee0508582a
JiYoungPark <aksend82@daum.net>
2022-08-22 15:54:54 +0900

    expr 키워드 기능 추가

--- source/ActionInzentProject/APIInzentTranAction.js
+++ source/ActionInzentProject/APIInzentTranAction.js
@@ -398,12 +398,13 @@ if (!nexacro.APIInzentTranAction)
 			sIOType		= oModel["iotype"];
 			oFieldList	= oModel["fieldlist"];
 			
+			// Model이 사용된 View 객체
 			oView		= objForm._findComponentForArrange(sViewId);
 			
 			if (oView)
 			{
-				oViewDataset = oView.getViewDataset();
-				nRow = oViewDataset.rowposition ? oViewDataset.rowposition : 0;
+				oViewDataset = oView.getViewDataset();								// viewdataset
+				nRow = oViewDataset.rowposition ? oViewDataset.rowposition : 0;		// rowposition
 				
 				if (oViewDataset && oViewDataset._type_name == "Dataset")
 				{
@@ -411,6 +412,7 @@ if (!nexacro.APIInzentTranAction)
 					{
 						oField = oFieldList[j];
 						
+						// Field의 value값 반환
 						sFieldValue = this.gfnGetFieldValue(oField, oView, oViewDataset);
 						
 						// 데이터 셋팅
@@ -422,7 +424,7 @@ if (!nexacro.APIInzentTranAction)
 	};
 	
 	// Field의 value값 반환
-	nexacro.APIInzentTranAction.prototype.gfnGetFieldValue = function(oField)
+	nexacro.APIInzentTranAction.prototype.gfnGetFieldValue = function(oField, oView)
 	{
 		var sReturnValue;
 		
@@ -433,20 +435,74 @@ if (!nexacro.APIInzentTranAction)
 		
 		if (this.gfnIsNull(sFieldValue))		return;
 		
-		var arrFieldValue = sFieldValue.toString().split(":");
-		var sType = arrFieldValue[0].toLowerCase();
+		var sType = sFieldValue.toString().substr(0,5).toLowerCase();
 		
 		switch (sType)
 		{
-			case "expr":
-				sReturnValue = eval(arrFieldValue[1]);
+			case "expr:":
+				var sExprText = sFieldValue.toString().substr(5);
+				
+				// expr 전환시 기준이 될 view name
+				var sViewNm = oView ? oView.name : this.targetview;
+				
+				// expr Text 처리
+				sExprText = this.gfnGetExprText(sExprText,sViewNm);
+				
+				sReturnValue = eval(sExprText);
 				break;
 			default:
 				sReturnValue = sFieldValue;
 				break;
 		}
 		
+		//this.gfnLog(sFieldName + " : " +sReturnValue);
+		
 		return sReturnValue;
+	};
+	
+	// expr Text 처리
+	nexacro.APIInzentTranAction.prototype.gfnGetExprText = function(sExprText, sViewNm)
+	{
+		var sRetText = sExprText;
+		
+		// 1) '['와 ']' 사이값 추출
+		// [field] 형식 : targetview의 viewdataset field컬럼값 반환 
+		// [view:field] 형식 : view의 viewdataset field컬럼값 반환 
+		var regEx = /(?<=\[)(.*?)(?=\])/g;
+		var sMatch;
+		var sView;
+		var sViewDataset;
+		var sColumnId;
+		var sReText;
+		
+		// 변환처리
+		while ((m = regEx.exec(sRetText)) !== null)
+		{
+			// This is necessary to avoid infinite loops with zero-width matches
+			if (m.index === regEx.lastIndex) {
+				regEx.lastIndex++;
+			}
+			
+			sMatch = m[0];
+			
+			var arrMatch = sMatch.split(":");
+			if (arrMatch.length < 2) {		// view설정안됨
+				sView		= sViewNm;
+				sColumnId	= arrMatch[0];
+			} else {
+				sView		= arrMatch[0];
+				sColumnId	= arrMatch[1];
+			}
+			
+			sViewDataset = "this.parent." + sView + ".form.viewdataset";
+			
+			// 변환처리 : this.parent.[view].form.viewdataset.getColumn(this.parent.[view].form.viewdataset.rowposition,'[field]')
+			sReplace = sViewDataset + ".getColumn(" + sViewDataset + ".rowposition,'" + sColumnId + "')";
+			
+			sRetText = sRetText.replace("[" + sMatch + "]",sReplace);
+		}
+		
+		return sRetText;
 	};
 	
 	// User Argument 처리 : transaction Argument로 추가
@@ -462,7 +518,23 @@ if (!nexacro.APIInzentTranAction)
 		if (!oExtraList)
             return;
 		
-		oExtraList.forEach(oExtra => sReturnValue += " " + oExtra["name"] + "=" + nexacro.wrapQuote(oExtra["value"]));
+		var oExtra;
+		var sExtraName;
+		var sExtraValue;
+		
+		// oExtraList객체값을 transaction argument 형식으로 변환
+		//oExtraList.forEach(oExtra => sReturnValue += " " + oExtra["name"] + "=" + nexacro.wrapQuote(oExtra["value"]));
+		for (var i = 0; i < oExtraList.length; i++)
+		{
+			oExtra = oExtraList[i];
+			sExtraName = oExtra["name"];
+			
+			// Field의 value값 반환
+			sExtraValue = this.gfnGetFieldValue(oExtra);
+			
+			// transaction argument 형식으로 셋팅
+			sReturnValue += " " + sExtraName + "=" + nexacro.wrapQuote(sExtraValue);
+		}
 		
 		sReturnValue = sReturnValue.substr(1);
 		//this.gfnLog(sReturnValue);
