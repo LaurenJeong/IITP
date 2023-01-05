7c1d8c8d99a32ac3e6b462fe6349dcb1303f1f50
JiYoungPark <aksend82@daum.net>
2023-01-05 17:43:59 +0900

    ActionAsset 변경사항 적용

---
+++ source/QuickCode/_extlib_/ActionAsset/LibAsset.js
@@ -0,0 +1,20 @@
+var pAction = nexacro.Action.prototype;
+
+//===============================================================
+// nexacro.Action : 변수선언 부분(변경불가)
+//===============================================================
+// 대상 Action : PopupAction, PopupActionM
+pAction._POPUP_CALLBACK = "fnPopupActionCallback";			// 팝업 Callback함수(Action 내부에서 사용)
+
+// 대상 Action : TransactionAction
+pAction._TRAN_CALLBACK_NM = "fnTranActionCallback";			// Action공통 Callback함수명
+
+//===============================================================
+// nexacro.Action : 변수선언 부분(프로젝트마다 변경)
+//===============================================================
+// 대상 Action : ExcelExportAction, ExcelImportAction
+pAction._COM_EXCEL_URL = "svc::XExportImport.do";			// XENI URL
+
+// 대상 Action : DsSetFirstCdAction
+pAction._COM_CODE_COL = "COMN_CD";							// 공통코드 코드컬럼명
+pAction._COM_NAME_COL = "COMN_CD_NM";						// 공통코드 코드명컬럼명
